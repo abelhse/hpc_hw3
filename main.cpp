eff4c071f6b447995878198500f4ff3847ffee04
aibelov_1 <aibelov_1@sms>
2024-12-08 15:35:35 +0300

    похоже все работает, но я выставил параметры поточнее. на тех параметрах что требуется в дз точность не очень

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-#import <iostream>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <fstream>
 
 using std::endl;
 using std::cout;
 using std::swap;
+using std::vector;
+using std::max;
+using std::ofstream;
 
 
 double u_true(
@@ -14,11 +20,11 @@ double u_true(
     int M
 ) 
 {
-    int u = 0;
+    double u = 0;
     for (int m = 0; m <= M; m++) {
-        float c0 = pi * (2*m + 1) / l;
+        float c0 = M_PI * (2*m + 1) / l;
         float c1 = exp(-k * t * c0 * c0) / (2*m + 1);
-        u += (4 * u0 / pi) * c1 * sin(c0 * x);
+        u += (4 * u0 / M_PI) * c1 * sin(c0 * x);
     }
     return u;
 }
@@ -26,40 +32,58 @@ double u_true(
 
 int main(int argc, char **argv)
 {
-    // h = 0.02
+    double u0 = 1;
+
+    // // h = 0.02
+    // double l = 1;
+    // int Nx = 50 + 1;
+    // double h = l / (Nx - 1);
+
+    // // tau = 0.0002
+    // double T = 0.1;
+    // double Nt = 500 + 1;
+    // double tau = T / (Nt - 1);
+
     double l = 1;
-    int Nx = 50 + 1;
+    int Nx = 200 + 1;
     double h = l / (Nx - 1);
 
-    // tau = 0.0002
-    double T = 0.1
-    double Nt = 500 + 1;
+    double T = 0.1;
+    double Nt = 10000 + 1;
     double tau = T / (Nt - 1);
 
     double k = 1;
 
-    vector<double> u(1 + Nx + 1);
+    vector<double> u(1 + Nx + 1, 1);
     u[0] = 0;
     u[Nx + 1] = 0;
-    vector<double> u_new(1 + Nx + 1);
+    vector<double> u_new(1 + Nx + 1, 1);
     u_new[0] = 0;
     u_new[Nx + 1] = 0;
 
+    ofstream of_u_true("u_true.txt");
+    ofstream of_u("u.txt");
+
     for (int j = 0; j < Nt; j++) {
+        // cout << "j=" << j << endl;
         for (int i = 1; i <= Nx; i++) {
             u_new[i] = u[i] + (k * tau) / (h * h) * (u[i+1] - 2*u[i] + u[i-1]);
+            of_u << u_new[i] << " ";
         }
         swap(u, u_new);
+
+        double maxAbsErr = 0;
+        for (int i = 1; i <= Nx; i++) {
+            // double t = T;
+            double t = j * tau;
+            double x = 0 + (i-1)*h;
+            double u_true_i = u_true(x, t, k, l, u0, 25);
+            of_u_true << u_true_i << " ";
+            maxAbsErr = max(maxAbsErr, fabs(u[i] - u_true_i));
+        }
+        // cout << "maxAbsErr=" << maxAbsErr << endl;
+
+        of_u_true << endl;
+        of_u << endl;
     }
-
-    double maxAbsErr = 0;
-    for (int i = 1; i <= Nx; i++) {
-        double t = T;
-        double x = 0 + (i-1)*h;
-        u_true_i = u_true(x, t, k, l, u0, 5);
-        cout << u[i] << "\t" << u_true_i << endl;
-        maxAbsErr = max(maxAbsErr, abs(u[i] - u_true_i));
-    }   
-
-
 }
